d7f40228873f7badbded62d1c185258d42d76885
Martin Masson <martin.masson@datastorm.fr>
2024-11-19 19:35:36 +0100

    ENH: Mock alentours cards

--- src/components/appbars/AccueilAppBar.vue
+++ src/components/appbars/AccueilAppBar.vue
@@ -7,12 +7,11 @@
 </script>
 <template>
     <v-app-bar prominent >
+      <v-app-bar-title v-if="!isMobile">A l'Ombre des Oiseaux</v-app-bar-title>
       <div v-if="isMobile" style="color:teal; margin-left: 20px;">
         <v-icon icon="mdi-bird"></v-icon>
       </div>
-      <div v-else>
-        <v-app-bar-title>A l'Ombre des Oiseaux</v-app-bar-title>
-      </div>
+
       <v-spacer></v-spacer>
 
       <v-btn icon>
--- src/pages/Alentours.vue
+++ src/pages/Alentours.vue
@@ -5,9 +5,36 @@
                 v-for="(value, name, index) in this.tabs"
                 :key="name"
                 >
-                <v-card class="d-flex justify-center align-center" height="100%">
-                  <span class="text-h2">{{ value }}</span>
-                </v-card>
+                    <v-card
+                        class="mx-auto"
+                        max-width="400"
+                        style="margin-top: 20px;margin-bottom: 20px;"
+                    >
+                        <v-img
+                        class="align-end text-white"
+                        height="200"
+                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        cover
+                        >
+                        <v-card-title> {{ value }}</v-card-title>
+                        </v-img>
+
+                        <v-card-subtitle class="pt-4">
+                        Number 10
+                        </v-card-subtitle>
+
+                        <v-card-text>
+                        <div>Whitehaven Beach</div>
+
+                        <div>Whitsunday Island, Whitsunday Islands</div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                        <v-btn color="orange" text="Share"></v-btn>
+
+                        <v-btn color="orange" text="Explore"></v-btn>
+                        </v-card-actions>
+                    </v-card>
             </v-tabs-window-item>
         </v-tabs-window>
     </div>
@@ -18,11 +45,11 @@
         name: 'Alentours',
         data: () => ({
             tabs: {
-                "#commerces": 'c',
-                "#bazoches": "b",
-                "#villages": 'v',
-                "#villes": "V",
-                "#sports": "s"
+                "#commerces": 'Boulangerie',
+                "#bazoches": "Illumination de noël de Vimpelles",
+                "#villages": 'Moret sur Loing',
+                "#villes": "Troyes",
+                "#sports": "Promenade le long de l'Yonne"
             },
             tab: 0,
         }),  
