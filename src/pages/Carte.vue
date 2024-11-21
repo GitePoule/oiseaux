1e0d28fef2b2fa004d0bc7d753df6dadab257873
Martin Masson <martin.masson@datastorm.fr>
2024-11-21 14:51:48 +0100

    FEAT: Add button over map

--- src/pages/Carte.vue
+++ src/pages/Carte.vue
@@ -1,13 +1,29 @@
 <template>
-  <div style="height:100%; width:100%">
-    <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="[48.39644, 3.17230]">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-    </l-map>
-  </div>
+      <div div id="map" style="height: 500px; position: relative;">
+        <l-map v-model:zoom="zoom" :use-global-leaflet="false" :center="[48.39644, 3.17230]"
+          >
+          <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base" name="OpenStreetMap"
+          ></l-tile-layer>
+        </l-map>
+        <v-card class="map-button" style="position: absolute; top: 10px; left: 10%; width: 80%;">
+          <v-btn-toggle
+            v-model="toggle_multiple"
+            background-color="primary"
+            dark
+            multiple
+          >
+            <v-btn prepend-icon="mdi-castle">Châteaux</v-btn>
+            <v-btn>
+              <v-icon>mdi-format-italic</v-icon>
+            </v-btn>
+            <v-btn>
+              <v-icon>mdi-format-color-fill</v-icon>
+            </v-btn>
+          </v-btn-toggle>
+        </v-card>
+      </div>
 </template>
 
 <script>
@@ -28,5 +44,7 @@
 </script>
 
 <style scoped>
-/* Ajoutez vos styles ici */
+.map-button {
+  z-index: 1000; /* Assurez-vous que les boutons sont au-dessus de la carte */
+}
 </style>
