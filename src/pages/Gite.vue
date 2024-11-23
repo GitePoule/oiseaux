aa3348c447afde9d0f2fe00f914faec5965a0abb
Martin Masson <martin.masson@datastorm.fr>
2024-11-23 13:26:53 +0100

    FEAT: More info in gite page (floors, guides)

--- src/pages/Gite.vue
+++ src/pages/Gite.vue
@@ -24,11 +24,23 @@
   <div style="margin: 10px;">
 
     <br />
-    <h3>Gîte "A l'ombre des oiseaux"</h3>
+    <h3>A l'ombre des oiseaux</h3>
     <br />
     <p>Maison - Bazoches-Lès-Bray (77118)</p>
     <p>1 lit double - 2 lits simples</p>
+    <p style="font-size: 9pt;">Note Gîtes de France : 5/5 (Déc. 2024) </p>
+    <p style="font-size: 9pt;">Classement Gîtes de France : 3 épis  </p>
     <br />
+
+    <v-avatar color="green-darken-2" size="small" style="margin-right: 5px;">
+      <v-icon icon="mdi-barley"></v-icon>
+    </v-avatar>
+    <v-avatar color="green-darken-2" size="small" style="margin-right: 5px;">
+      <v-icon icon="mdi-barley"></v-icon>
+    </v-avatar>
+    <v-avatar color="green-darken-2" size="small" style="margin-right: 5px;">
+      <v-icon icon="mdi-barley"></v-icon>
+    </v-avatar>
 
     <v-chip
       class="ma-2"
@@ -43,28 +55,112 @@
     <br /><v-divider></v-divider>
     <h3>Rez-de-chausée</h3>
 
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-chef-hat"
+        subtitle=""
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Cuisine</span>
+        </template>
+    </v-card><br/>
+
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-television"
+        subtitle="Android TV"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Salon</span>
+        </template>
+    </v-card><br/>
+
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-shower"
+        subtitle="Douche"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Salle de bain</span>
+        </template>
+    </v-card><br/>
+
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-toilet"
+        subtitle="Papier toilette inclus"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Toilettes 1</span>
+        </template>
+    </v-card>
+
     <br /><v-divider></v-divider>
     <h3>L'étage</h3>
 
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-bed-king"
+        subtitle="1 lit double"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Chambre 1</span>
+        </template>
+    </v-card><br/>
+
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-bed"
+        subtitle="2 lits simples"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Chambre 2</span>
+        </template>
+    </v-card><br/>
+
+    <v-card
+        class="mx-auto"
+        prepend-icon="mdi-toilet"
+        subtitle="Papier toilette inclus"
+        width="200"
+    >
+        <template v-slot:title>
+        <span class="font-weight-black">Toilettes 2</span>
+        </template>
+    </v-card>
+
     <br /><v-divider></v-divider>
-    <h3>Les équipements du logement</h3>
-        <v-btn
-        size="x-large"
-        text="Click Me"
-        @click="sheet = !sheet"
-        ></v-btn>
-        <v-bottom-sheet inset v-model="sheet"><v-card>
+    <h3>Les équipements du logement</h3><br />
+
+        <v-chip v-for="(e) in equipements" :key="e.label"  style="margin: 5px;" size="small">
+            {{ e.label }}
+        </v-chip>
+        <v-chip
+            @click="sheetEquipmt = !sheetEquipmt"
+            size="small"
+            variant="elevated"
+            prepend-icon="mdi-format-list-bulleted"
+        >
+            Voir les équipements
+        </v-chip>
+
+        <v-bottom-sheet inset v-model="sheetEquipmt"><v-card>
             <v-card-text>
                 <v-row align="center" justify="center">
                     <v-btn
                         variant="text"
-                        @click="sheet = !sheet"
+                        @click="sheetEquipmt = !sheetEquipmt"
                     >
                         Fermer la liste
                     </v-btn>
                 </v-row>
             </v-card-text>
-
             <v-list>
                 <div v-for="(e, l, i) in equipements" :key="e">
                     <v-divider v-if="i != 0"></v-divider>
@@ -83,24 +179,53 @@
 
         </v-card></v-bottom-sheet>
 
-    <br /><v-divider></v-divider>
-    <h3>Petit guide pour le wifi</h3>
+    <br/><br/><br/><v-divider></v-divider>
+    <h3>Petits guides d'utilisation</h3><br/>
 
-    <br /><v-divider></v-divider>
-    <h3>Petit guide pour la télévision</h3>
+    <v-chip v-for="(e) in ['Four', 'Télévision', 'Wifi']" :key="e.label"  style="margin: 5px;" size="small">
+        {{ e }}
+    </v-chip>
+    <v-chip
+        @click="sheetGuide = !sheetGuide"
+        size="small"
+        variant="elevated"
+        prepend-icon="mdi-help-box"
+    >
+        Ouvrir les guides
+    </v-chip>
 
-    <br /><v-divider></v-divider>
-    <h3>Jeux de société</h3>
+    <v-bottom-sheet inset v-model="sheetGuide"><v-card>
+        <v-tabs
+            v-model="tabGuide"
+            align-tabs="center"
+            color="deep-purple-accent-4"
+            >
+            <v-tab prepend-icon="mdi-stove" :value="1"></v-tab>
+            <v-tab prepend-icon="mdi-television" :value="2"></v-tab>
+            <v-tab prepend-icon="mdi-wifi" :value="3"></v-tab>
+            <v-tab prepend-icon="mdi-close" @click="sheetGuide = !sheetGuide" ></v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tabGuide">
+            <v-tabs-window-item :value="1" height="600px">Guide 1</v-tabs-window-item>
+            <v-tabs-window-item :value="2" height="600px">Guide 2</v-tabs-window-item>
+            <v-tabs-window-item :value="3" height="600px">Guide 3</v-tabs-window-item>
+        </v-tabs-window>
+
+    </v-card></v-bottom-sheet>
+
+    <br /><br /><v-divider></v-divider>
+    <h3>Jeux de société</h3><br />
     <p>Les jeux de société sont disponibles à la demande.</p>
 
     <br /><v-divider></v-divider>
-    <h3>Jardin extérieur</h3>
+    <h3>Jardin extérieur</h3><br />
 
     <br /><v-divider></v-divider>
-    <h3>Règlement intérieur</h3>
+    <h3>Règlement intérieur</h3><br />
 
     <br /><v-divider></v-divider>
-    <h3>Sécurité du logement</h3>
+    <h3>Sécurité du logement</h3><br />
 
 </div>
 </template>
@@ -110,7 +235,9 @@
     export default {
         name: 'Gite',
         data: () => ({
-          sheet: true,
+          tabGuide: 1,
+          sheetEquipmt: false,
+          sheetGuide: false,
           equipements: equipements
         })
     };
