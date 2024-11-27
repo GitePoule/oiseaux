833eefc780d509752dd2a666e3fc93fb8ee1fa94
Martin MASSON <martin.masson@protonmail.com>
2024-11-27 23:50:53 +0100

    FIX: Update index.js

--- src/assets/img/travaux/index.js
+++ src/assets/img/travaux/index.js
@@ -16,4 +16,4 @@ const imagesTravaux = [
   image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13
 ];
 
-export default images;
+export default imagesTravaux;
