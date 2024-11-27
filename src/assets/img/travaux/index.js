a5adf943993b254bdd144cd9415442f6e9799efd
Martin MASSON <martin.masson@protonmail.com>
2024-11-27 23:43:42 +0100

    ENH: Ads index.js in assets/IMG/travaux

---
+++ src/assets/img/travaux/index.js
@@ -0,0 +1,19 @@
+import image1 from '@/assets/img/travaux/IMG_20200315_142746.jpg';
+import image2 from '@/assets/img/travaux/IMG_20200315_142753.jpg';
+import image3 from '@/assets/img/travaux/IMG_20200315_142814.jpg';
+import image4 from '@/assets/img/travaux/IMG_20200315_142818.jpg';
+import image5 from '@/assets/img/travaux/IMG_20200315_142819.jpg';
+import image6 from '@/assets/img/travaux/IMG_20200315_142823.jpg';
+import image7 from '@/assets/img/travaux/IMG_20200315_142826.jpg';
+import image8 from '@/assets/img/travaux/IMG_20200315_142833.jpg';
+import image9 from '@/assets/img/travaux/IMG_20200315_142838.jpg';
+import image10 from '@/assets/img/travaux/IMG_20200315_142843.jpg';
+import image11 from '@/assets/img/travaux/IMG_20200315_142857.jpg';
+import image12 from '@/assets/img/travaux/IMG_20200315_142859.jpg';
+import image13 from '@/assets/img/travaux/IMG_20241127_232700.jpg';
+
+const imagesTravaux = [
+  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13
+];
+
+export default images;
