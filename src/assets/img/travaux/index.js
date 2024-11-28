ef3b64b3626ed38284a759087ec0752d67b16ca0
Martin Masson <martin.masson@datastorm.fr>
2024-11-28 10:09:04 +0100

    ENH: Update index.js for assers/img/travaux

--- src/assets/img/travaux/index.js
+++ src/assets/img/travaux/index.js
@@ -1,19 +1,28 @@
-import image1 from '@/assets/img/travaux/IMG_20200315_142746.jpg';
-import image2 from '@/assets/img/travaux/IMG_20200315_142753.jpg';
-import image3 from '@/assets/img/travaux/IMG_20200315_142814.jpg';
-import image4 from '@/assets/img/travaux/IMG_20200315_142818.jpg';
-import image5 from '@/assets/img/travaux/IMG_20200315_142819.jpg';
-import image6 from '@/assets/img/travaux/IMG_20200315_142823.jpg';
-import image7 from '@/assets/img/travaux/IMG_20200315_142826.jpg';
-import image8 from '@/assets/img/travaux/IMG_20200315_142833.jpg';
-import image9 from '@/assets/img/travaux/IMG_20200315_142838.jpg';
-import image10 from '@/assets/img/travaux/IMG_20200315_142843.jpg';
-import image11 from '@/assets/img/travaux/IMG_20200315_142857.jpg';
-import image12 from '@/assets/img/travaux/IMG_20200315_142859.jpg';
-import image13 from '@/assets/img/travaux/IMG_20241127_232700.jpg';
+import image1 from '@/assets/img/travaux/IMG_20200315_142819.jpg';
+import image2 from '@/assets/img/travaux/IMG_20200315_142814.jpg';
+import image3 from '@/assets/img/travaux/IMG_20200315_142859.jpg';
+import image4 from '@/assets/img/travaux/IMG_20200315_142838.jpg';
+import image5 from '@/assets/img/travaux/IMG_20200315_142613.jpg';
+import image6 from '@/assets/img/travaux/IMG_20200315_142709.jpg';
+import image7 from '@/assets/img/travaux/IMG_20200315_142753.jpg';
+import image8 from '@/assets/img/travaux/IMG_20200315_142718.jpg';
+import image9 from '@/assets/img/travaux/IMG_20200315_142818.jpg';
+import image10 from '@/assets/img/travaux/IMG_20200315_142833.jpg';
+import image11 from '@/assets/img/travaux/IMG_20241127_232700.jpg';
+import image12 from '@/assets/img/travaux/IMG_20200315_142823.jpg';
+import image13 from '@/assets/img/travaux/IMG_20200315_142725.jpg';
+import image14 from '@/assets/img/travaux/IMG_20200315_142728.jpg';
+import image15 from '@/assets/img/travaux/IMG_20200315_142826.jpg';
+import image16 from '@/assets/img/travaux/IMG_20200315_142746.jpg';
+import image17 from '@/assets/img/travaux/IMG_20200315_142706.jpg';
+import image18 from '@/assets/img/travaux/IMG_20200315_142712.jpg';
+import image19 from '@/assets/img/travaux/IMG_20200315_142903.jpg';
+import image20 from '@/assets/img/travaux/IMG_20200315_142843.jpg';
+import image21 from '@/assets/img/travaux/IMG_20200315_142857.jpg';
 
 const imagesTravaux = [
-  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13
+  image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
+  image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21
 ];
 
 export default imagesTravaux;
