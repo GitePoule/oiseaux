53d1fb919c33d3387c2e5e989e5ac2a9ea16dd93
Martin MASSON <martin.masson@protonmail.com>
2024-12-03 08:14:22 +0100

    CLEAN: Remove draft marker

--- src/pages/Carte.vue
+++ src/pages/Carte.vue
@@ -69,24 +69,7 @@
             zoom: 12,
             isMobile: false,
             filters: [],
-            markers: [
-                  {
-                    "id": "essai",
-                    "categ": "activity",
-                    "name": "Essai",
-                    "img": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-                    "imgSize": [25, 25],
-                    "coord": [48.1759, 3.18890],
-                  },
-                  {
-                    "id": "essai2",
-                    "categ": "activity",
-                    "name": "Essai",
-                    "imgSize": [25, 25],
-                    "img": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-                    "coord": [48.39759, 3.18990],
-                  },
-            ],
+            markers: [],
             hiddenMarkers: {}
         }),
         mounted() {
