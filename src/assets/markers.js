924a6ca6b3114b24559fd222d9f0c38ea6841031
Martin MASSON <martin.masson@protonmail.com>
2024-12-03 08:15:23 +0100

    ENH: Add markers.js

---
+++ src/assets/markers.js
@@ -0,0 +1,20 @@
+const markers = [
+                  {
+                    "id": "essai",
+                    "categ": "activity",
+                    "name": "Essai",
+                    "img": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+                    "imgSize": [25, 25],
+                    "coord": [48.1759, 3.18890],
+                  },
+                  {
+                    "id": "essai2",
+                    "categ": "activity",
+                    "name": "Essai",
+                    "imgSize": [25, 25],
+                    "img": "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+                    "coord": [48.39759, 3.18990],
+                  },
+            ]
+
+export default markers;
